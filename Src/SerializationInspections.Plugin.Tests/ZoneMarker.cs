1cb4608dec1a16d911a07a8a44132b835849cd7a
Ulrich Buchgraber <ulrich.b@gmx.at>
2015-09-03 22:13:41 +0200

    Fixed ZoneMarkers: Removed unnecessary IRequire<> in the test project (+ fixed namespaces), and moved the IRequire<ICodeEditingZone> down to the Quickfixes namespace because it was too restrictive in the root namespace (issue with InspectCode)

--- Src/SerializationInspections.Plugin.Tests/ZoneMarker.cs
+++ Src/SerializationInspections.Plugin.Tests/ZoneMarker.cs
@@ -1,29 +1,27 @@
 ﻿using JetBrains.Application.BuildScript.Application.Zones;
-using JetBrains.ReSharper.Feature.Services;
-using JetBrains.ReSharper.Psi.CSharp;
 using JetBrains.ReSharper.TestFramework;
 using JetBrains.TestFramework;
 using JetBrains.TestFramework.Application.Zones;
 using NUnit.Framework;
-using XmlDocInspections.Plugin.Tests;
+using SerializationInspections.Plugin.Tests;
 
 [assembly: TestDataPathBase(@"..\SerializationInspections.Sample")]
 
-namespace XmlDocInspections.Plugin.Tests
+namespace SerializationInspections.Plugin.Tests
 {
     [ZoneDefinition]
-    public interface IXmlDocInspectionsTestEnvironmentZone : ITestsZone, IRequire<PsiFeatureTestZone>
+    public interface ISerializationInspectionsTestEnvironmentZone : ITestsZone, IRequire<PsiFeatureTestZone>
     {
     }
 
     [ZoneMarker]
-    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<IXmlDocInspectionsTestEnvironmentZone>
+    public class ZoneMarker : IRequire<ISerializationInspectionsTestEnvironmentZone>
     {
     }
 }
 
 // ReSharper disable once CheckNamespace
 [SetUpFixture]
-public class TestEnvironmentSetUpFixture : ExtensionTestEnvironmentAssembly<IXmlDocInspectionsTestEnvironmentZone>
+public class TestEnvironmentSetUpFixture : ExtensionTestEnvironmentAssembly<ISerializationInspectionsTestEnvironmentZone>
 {
 }
---
+++ Src/SerializationInspections.Plugin/Quickfixes/ZoneMarker.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+
+namespace SerializationInspections.Plugin.Quickfixes
+{
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>
+    {
+    }
+}
