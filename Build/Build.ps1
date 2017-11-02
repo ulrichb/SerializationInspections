8ac99226307bc33fa97a972cc84d52e89d3273cc
Ulrich Buchgraber <ulrich.b@gmx.at>
2017-11-03 00:18:49 +0100

    Refactor shared csproj stuff into Directory.Build.props

--- Build/Build.ps1
+++ Build/Build.ps1
@@ -18,14 +18,14 @@ $SolutionFilePath = "SerializationInspections.sln"
 $MSBuildPath = (Get-ChildItem "${env:ProgramFiles(x86)}\Microsoft Visual Studio\2017\*\MSBuild\15.0\Bin\MSBuild.exe").FullName
 $NUnitAdditionalArgs = "--x86 --labels=All --agents=1"
 $NUnitTestAssemblyPaths = @(
-    "Src\SerializationInspections.Plugin.Tests\bin\$Configuration\SerializationInspections.Plugin.Tests.dll"
+    "Src\SerializationInspections.Plugin.Tests\bin\R20172\$Configuration\SerializationInspections.Plugin.Tests.R20172.dll"
     "Src\SerializationInspections.Plugin.Tests\test\data\bin\$Configuration\SerializationInspections.Sample.dll"
 )
 $NUnitFrameworkVersion = "net-4.5"
 $TestCoverageFilter = "+[SerializationInspections*]* -[SerializationInspections*]ReSharperExtensionsShared.* -[SerializationInspections.Sample]*"
 $NuspecPath = "Src\SerializationInspections.Plugin\SerializationInspections.nuspec"
 $NugetPackProperties = @(
-    "Version=$(CalcNuGetPackageVersion 20172);Configuration=$Configuration;DependencyVer=[9.0];BinDirInclude=bin"
+    "Version=$(CalcNuGetPackageVersion 20172);Configuration=$Configuration;DependencyVer=[9.0];BinDirInclude=bin\R20172"
 )
 $NugetPushServer = "https://www.myget.org/F/ulrichb/api/v2/package"
 
